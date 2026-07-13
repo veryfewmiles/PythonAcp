c5b99a0160bd6626174317cd65aa77f629d66c23
veryfewmiles <ramzalrbet@gmail.com>
2026-07-13 11:29:30 +1000

    initial commit

---
+++ aboutme.py
@@ -0,0 +1,10 @@
+name = "Abeer"
+age = 11
+isstudent = True
+weight = 40
+
+print("About myself")
+print("name:", name)
+print("age:", age)
+print("student:", isStudent)
+print("weight:", weight "kg")
