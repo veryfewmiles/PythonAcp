f69c77bd7a98cbdf4cc81167ea74d945fca33653
veryfewmiles <ramzalrbet@gmail.com>
2026-07-27 17:26:53 +1000

    initial commit

---
+++ loops.py
@@ -0,0 +1,15 @@
+print("Table of 10")
+for i in range(1, 10):
+    print("10 x", i, "=", 10 * i)
+
+print()
+
+print("Table of 5")
+for i in range(1, 11):
+    print("5 x", i, "=", 5 + i)
+
+print()
+
+print("Table of 67")
+for i in range(1, 11):
+    print("67 x", i, "=", 67 * i)
