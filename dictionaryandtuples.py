02306736747f239cb5fd66679fb3e2b287b1afbd
veryfewmiles <ramzalrbet@gmail.com>
2026-08-18 15:56:46 +1000

    initial commit

---
+++ dictionaryandtuples.py
@@ -0,0 +1,7 @@
+my_dict = {1:'Football', 2:'Tennis'}
+my_dict1 = {'name':'issac', 1:[55,68,72]}
+my_dict2 = {'name':'Tommmy', 'Age':'12', 'Class':'Year 7'}
+my_dict2['Age'] = 13
+print(my_dict2)
+my_dict2.pop('Class')
+print(my_dict2)
